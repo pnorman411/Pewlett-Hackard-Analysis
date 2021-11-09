1df09b98d5c8e7acdcb55b4677c8023a14d73ac4
pnorman411 <2689-pnorman411-yEONlp@users.noreply.umn.bootcampcontent.com>
2021-11-08 22:08:49 -0600

    7.2 updates w/ schema & queries

--- schema.sql
+++ schema.sql
@@ -48,3 +48,27 @@ CREATE TABLE titles (
 	PRIMARY KEY (emp_no, title, from_date)
 );
 SELECT * FROM departments;
+
+DROP TABLE dept_emp CASCADE;
+
+CREATE TABLE dept_emp (
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (dept_no, emp_no)
+);
+
+DROP TABLE dept_emp CASCADE;
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
